3102eeae6bad8526e202c6c0b580150895fb5997
Jesse Wang <jessewang828@gmail.com>
2025-03-16 18:56:57 +0000

    creating train.py

---
+++ data/note.md
@@ -0,0 +1,5 @@
+Download the fraudTrain.csv file from 
+
+https://www.kaggle.com/datasets/kartik2112/fraud-detection?resource=download&select=fraudTrain.csv
+
+and place it within this folder.
---
+++ src/train.py
@@ -0,0 +1,69 @@
+# run this script to generate rf_model_final.pkl file in the /models directory
+# this trains a random forest model on the full fraudTrain.csv dataset
+# there is a separate test set available on Kaggle
+
+import os
+import joblib
+import pandas as pd
+import time
+from typing import Tuple
+from pipeline import Preprocessor
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
+from utils import create_timestamp_columns
+
+
+
+def preprocess(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series]:
+    """
+    Preprocesses the raw data by transforming features and extracting the target variable.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The raw input data containing features and target labels.
+
+    Returns
+    -------
+    X : pd.DataFrame
+        The preprocessed feature matrix with transformed features.
+    y : pd.Series
+        The target labels indicating fraud (1) or non-fraud (0).
+    """
+    start = time.time()
+    print(f'Preprocessing training data with {len(df)} samples')
+
+    # generate engineered timestamp columns
+    df = create_timestamp_columns(df)
+    X = df[Preprocessor.all_features]
+    y = df[Preprocessor.target]
+
+    # feature scaling and imputation
+    preprocessor = Preprocessor(X)
+    X = preprocessor.fit_transform()
+
+    # Oversample minority class using SMOTE
+    smote = SMOTE(sampling_strategy="auto", random_state=42)
+    X, y = smote.fit_resample(X, y)
+
+    print(f'Finished preprocessing data in {time.time() - start:.2f} seconds')
+    return X, y
+
+
+if __name__ == "__main__":
+
+    filepath = os.path.join("data", "fraudTrain.csv")
+    print(f"Importing raw data from {filepath}")
+    df = pd.read_csv(filepath)
+    X, y = preprocess(df)
+
+    start = time.time()
+    n_estimators, max_depth = 50, 5
+    print(f"Training random forest classifier with {n_estimators=} and {max_depth=}")
+    clf = RandomForestClassifier(n_estimators=50, max_depth=5, n_jobs=-1)
+    clf.fit(X, y)
+    print(f'Finished training classifier in {time.time() - start:.2f} seconds')
+
+    filename = "rf_model_final.pkl"
+    joblib.dump(clf, f"models/{filename}")
+    print("Model saved!")
